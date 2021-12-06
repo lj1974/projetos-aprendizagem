7bd2cfc49d54f3107954379ec3ded0ef0cd9e28d
LJ <84136544+lj1974@users.noreply.github.com>
2021-12-06 19:37:36 -0300

    greylake starts

---
+++ C++/Greylake_Hospital/greylake.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#define limpar system("cls");
+#define setbuf setbuf(stdin, NULL);
+using namespace std;
+
+class HospitalMain {
+
+public:
+    void SetHall();
+
+private:
+
+protected:
+};
+
+void HospitalMain::SetHall(){
+    cout << "\n\n\tWelcome To GREYLAKE register center.\n";
+    cout << "\tTake the necessary steps to fill your registration.\n";
+}
+
